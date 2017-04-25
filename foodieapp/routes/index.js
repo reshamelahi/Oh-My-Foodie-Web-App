38341fa6fc7417189a4c970701abaccb661251a7
Shweta <shwayytha@gmail.com>
2017-04-25 17:41:46 -0400

    fix foodienetwork

--- foodieapp/routes/index.js
+++ foodieapp/routes/index.js
@@ -101,24 +101,22 @@ router.post('/foodienetwork', (req, res) => {
 
 router.get('/foodienetwork/:slug', function(req, res) {
 	Link.findOne({slug: req.params.slug}, (err, links) => {
-    const latestComment = req.session.lastComment || '';
     if (req.query.button === "Vote") {
       Link.findOneAndUpdate({slug: req.params.slug}, {$inc: {votes: 1}}, (err, links) => {
         res.redirect("/foodienetwork");
       });
     }
-		else { res.render('commentform', {links: links, 'lastComment': latestComment}); }
+		else { res.render('commentform', {links: links}); }
 	});
 });
 
 router.post('/foodienetwork/:slug', (req, res) => {
   const slugVar = req.params.slug;
-  Link.findOneAndUpdate({slug: slugVar}, {$push: {comments: {text: req.body.text, name:req.body.name}}}, (err, links) => {
+  Link.findOneAndUpdate({slug: slugVar}, {$push: {comments: {text: req.body.text, name: req.body.name}}}, (err, links) => {
     if(err) {
         res.render('commentform', {links:links, err:err}); 
     }
     else {
-      req.session.lastComment = req.body.text;
       res.redirect('/foodienetwork/' + slugVar);
     }
   });

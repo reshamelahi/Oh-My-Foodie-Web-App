f1421678c8adf50cc1f239152c645ae57ac00f6c
Resham Elahi <reshamelahi@172-16-212-192.DYNAPOOL.NYU.EDU>
2017-04-27 20:35:33 -0400

    fb auth for deployment again

--- foodieapp/auth.js
+++ foodieapp/auth.js
@@ -2,22 +2,30 @@
 
 // expose our config directly to our application using module.exports
 module.exports = {
+	"development": {
+	    'facebookAuth' : {
+	        'clientID'      : '1058720764230103', // your App ID
+	        'clientSecret'  : 'c59d0ea9b8dd3c182abc0f2cd7479ae7', // your App Secret
+	        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+	    },
+	    mongodb: 'mongodb://localhost/foodieproject'
+	},
 
-    'facebookAuth' : {
-        'clientID'      : '1058720764230103', // your App ID
-        'clientSecret'  : 'c59d0ea9b8dd3c182abc0f2cd7479ae7', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
-    },
+	"production_shweta": {
+	    'fbAuth_shweta' : {
+	        'clientID'      : '1058720764230103', // your App ID
+	        'clientSecret'  : 'c59d0ea9b8dd3c182abc0f2cd7479ae7', // your App Secret
+	        'callbackURL'   : 'http://linserv1.cims.nyu.edu:13575/auth/facebook/callback'
+	    },
+	    mongodb: process.env.MONGO_URI
+	},
 
-    'fbAuth_shweta' : {
-        'clientID'      : '1058720764230103', // your App ID
-        'clientSecret'  : 'c59d0ea9b8dd3c182abc0f2cd7479ae7', // your App Secret
-        'callbackURL'   : 'http://linserv1.cims.nyu.edu:13575/auth/facebook/callback'
-    },
-
-    'fbAuth_Resham' : {
-        'clientID'      : '1058720764230103', // your App ID
-        'clientSecret'  : 'c59d0ea9b8dd3c182abc0f2cd7479ae7', // your App Secret
-        'callbackURL'   : 'http://linserv1.cims.nyu.edu:12873/auth/facebook/callback'
-    }
+	"production_resham": {
+	    'fbAuth_Resham' : {
+	        'clientID'      : '1058720764230103', // your App ID
+	        'clientSecret'  : 'c59d0ea9b8dd3c182abc0f2cd7479ae7', // your App Secret
+	        'callbackURL'   : 'http://linserv1.cims.nyu.edu:12873/auth/facebook/callback'
+	    },
+	    mongodb: process.env.MONGO_URI
+	}
 };
